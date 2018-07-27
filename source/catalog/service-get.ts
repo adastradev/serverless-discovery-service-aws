c38441731c3d43ab1374c496b73c50430dce1c73
Andrew Regier <aregier@aais.com>
2018-07-26 22:35:17 -0500

    Quick test against dynamodblocal

--- source/catalog/service-get.ts
+++ source/catalog/service-get.ts
@@ -1,9 +1,19 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 import CatalogServiceModel from './model/CatalogServiceModel';
+import {DataMapper} from '@aws/dynamodb-data-mapper';
+import * as DynamoDB from 'aws-sdk/clients/dynamodb';
 
-export const main: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+export const main: Handler = async (event: APIGatewayEvent, context: Context, cb: Callback) => {
 
-  const model = new CatalogServiceModel();
+  const service = new CatalogServiceModel();
+  service.stage = 'dev';
+  service.serviceName = 'core-service-tenants';
+
+  const client = new DynamoDB({region: 'us-east-1', endpoint: 'http://0.0.0.0:8000'});
+  const mapper = new DataMapper({client});
+  await mapper.ensureTableExists(CatalogServiceModel, {readCapacityUnits: 1, writeCapacityUnits: 1})
+  await mapper.put({item: service});
+  console.log('Created service: ' + service.id);
 
   const response = {
     body: JSON.stringify({
