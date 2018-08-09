7209b2497d9487bf76dd5e9a2f7195f953d26b6c
Andrew Regier <aregier@regiernet.com>
2018-08-09 12:30:52 -0500

    Provision/deprovision tables automatically from the CD pipeline

---
+++ source/deprovision.ts
@@ -0,0 +1,14 @@
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import CatalogServiceController from './catalog/controller/CatalogServiceController';
+import createErrorResponse from './catalog/controller/createErrorResponse';
+
+export const main: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
+    try {
+        const controller = new CatalogServiceController();
+        const response = await controller.deprovisionTables();
+        callback(null, response);
+    } catch (Error) {
+        console.log(Error.message);
+        callback(null, createErrorResponse(501, Error.message));
+    }
+};
