35e1223a308648f213e722d3daba6122e0e9b8dc
Tim Rupe <trupe@aais.com>
2019-04-24 10:05:26 -0500

    Added remaining unit test scenarios. (scenarios will be updated)

--- test/unit/CatalogServiceController.spec.ts
+++ test/unit/CatalogServiceController.spec.ts
@@ -38,9 +38,92 @@ describe('CatalogServiceController', () => {
 
     describe('Scenario 4', () => {
         it('Test 1', () => {
-            const result = controller.filterVersion('1.x.x', tenantId1, 'dev', testTerm);
+            const result = controller.filterVersion('1', tenantId1, 'dev', testTerm);
             expect(result.ServiceURL).equals('http://url11.test.com/feat112');
         });
     });
 
+    describe('Scenario 5', () => {
+        it('Test 1', () => {
+            const result = controller.filterVersion('1.0.1', tenantId1, 'dev', testTerm);
+            expect(result.ServiceURL).equals('http://url10.test.com/feat101');
+        });
+    });
+
+    describe('Scenario 6', () => {
+        it('Test 1', () => {
+            const result = controller.filterVersion('2.0.0', tenantId1, 'dev', testTerm);
+            expect(result.ServiceURL).equals('http://url12.test.com/feat201');
+        });
+    });
+
+    describe('Scenario 7', () => {
+        it('Test 1', () => {
+            const result = controller.filterVersion('3', tenantId1, 'dev', testTerm);
+            expect(result.ServiceURL).equals('http://url4.test.com/dev');
+        });
+    });
+
+    describe('Scenario 8', () => {
+        it('Test 1', () => {
+            const result = controller.filterVersion(undefined, tenantId3, 'dev', testTerm);
+            expect(result.ServiceURL).equals('http://url4.test.com/dev');
+        });
+    });
+
+    describe('Scenario 9', () => {
+        it('Test 1', () => {
+            const result = controller.filterVersion('1', undefined, undefined, testCampus);
+            expect(result.ServiceURL).equals('http://url17.test.com/feat262');
+        });
+
+        it('Test 2', () => {
+            const result = controller.filterVersion('2', undefined, undefined, testCampus);
+            expect(result.ServiceURL).equals('http://url17.test.com/feat262');
+        });
+    });
+
+    describe('Scenario 10', () => {
+        it('Test 1', () => {
+            expect(() => controller.filterVersion('3', undefined, undefined, testCampus)).to.throw();
+        });
+
+        it('Test 2', () => {
+            expect(() => controller.filterVersion('3.2.1', undefined, undefined, testCampus)).to.throw();
+        });
+    });
+
+    describe('Scenario 11', () => {
+        it('Test 1', () => {
+            expect(() => controller.filterVersion(undefined, tenantId1, 'invalid_stage', testTerm)).to.throw();
+        });
+
+        it('Test 2', () => {
+            expect(() => controller.filterVersion(undefined, undefined, 'invalid_stage', testTerm)).to.throw();
+        });
+    });
+
+    describe('Scenario 12', () => {
+        it('Test 1', () => {
+            expect(() => controller.filterVersion(undefined, undefined, undefined, testTerm)).to.throw();
+        });
+    });
+
+    describe('Scenario 13', () => {
+        // Outside the scope of unit tests
+    });
+
+    describe('Scenario 14', () => {
+        it('Test 1', () => {
+            const result = controller.filterVersion('2', undefined, undefined, testCampus);
+            expect(result.ServiceURL).equals('http://url17.test.com/feat262');
+        });
+    });
+
+    describe('Scenario 15', () => {
+        it('Test 1', () => {
+            const result = controller.filterVersion('2.6.1', undefined, undefined, testCampus);
+            expect(result.ServiceURL).equals('http://url16.test.com/feat261');
+        });
+    });
 });
