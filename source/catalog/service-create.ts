334fc2c2ed09fba4e06177e4ec2ad79fa2994dc4
Larry M. Sanders <lsanders@aais.com>
2019-06-14 08:52:23 -0500

    Add astra logger

--- source/catalog/service-create.ts
+++ source/catalog/service-create.ts
@@ -1,3 +1,4 @@
+import Log from '@adastradev/astra-logger';
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 import { CatalogServiceModel } from './model/CatalogServiceModel';
 import CatalogServiceController from './controller/CatalogServiceController';
@@ -5,13 +6,14 @@ import createErrorResponse from './controller/createErrorResponse';
 
 export const main: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
     try {
+        Log.config({ tenant_id: 'Discovery Service' });
         const controller = new CatalogServiceController();
         const json = JSON.parse(event.body);
         const service = Object.assign(new CatalogServiceModel(), json);
         const response = await controller.create(service);
         callback(null, response);
     } catch (Error) {
-        console.log(Error.message);
+        Log.error(Error.message, Error.stack);
         callback(null, createErrorResponse(501, Error.message));
     }
 };
--- source/catalog/service-delete.ts
+++ source/catalog/service-delete.ts
@@ -1,3 +1,4 @@
+import Log from '@adastradev/astra-logger';
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 import { CatalogServiceModel } from './model/CatalogServiceModel';
 import CatalogServiceController from './controller/CatalogServiceController';
@@ -5,12 +6,13 @@ import createErrorResponse from './controller/createErrorResponse';
 
 export const main: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
     try {
+        Log.config({ tenant_id: 'Discovery Service' });
         const controller = new CatalogServiceController();
         const service = Object.assign(new CatalogServiceModel(), { ServiceID: event.pathParameters.id });
         const response = await controller.delete(service);
         callback(null, response);
     } catch (Error) {
-        console.log(Error.message);
+        Log.error(Error.message, Error.stack);
         callback(null, createErrorResponse(501, Error.message));
     }
 };
